d0a620756180740d87f2b2a318d8a61a1d334be8
chris.auret <chris.auret@yahoo.co.uk>
2015-07-28 23:21:54 +1000

    First commit

---
+++ app.js
@@ -0,0 +1,22 @@
+
+
+	var world = document.getElementById( 'world' ),
+		viewport = document.getElementById( 'viewport' ),
+		worldXAngle = 0,
+		worldYAngle = 0,
+		d = 0;
+
+	window.addEventListener( 'mousemove', function( e ) {
+		worldYAngle = -(.5 - (e.clientX / window.innerWidth ) ) * 180;
+		worldXAngle = (.5 - (e.clientY / window.innerHeight ) ) * 180;
+		console.log(worldXAngle + ' ' + worldYAngle);
+		updateView();
+
+	} );
+
+	function updateView() {
+		world.style.transform = 'translateZ( ' + d + 'px ) \
+        rotateX( ' + worldXAngle + 'deg) \
+        rotateY( ' + worldYAngle + 'deg)';
+	}
+
